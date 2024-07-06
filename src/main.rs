2c918eced63053d2edf1442b84dc7e8f3fec4b37
witchbl4des <menzo.smit-adam@epitech.eu>
2024-07-07 00:11:59 +0200

    add routing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use axum::{ routing::get, Router, };
+use axum::{Router, routing::get, routing::post, routing::put, routing::delete};
+use tokio::net::TcpListener;
+mod routing;
+use routing::{root, get_user, post_user, get_user_by_id, put_user, delete_user};
 
 #[tokio::main]
-
 async fn main() {
-	let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-	axum::serve(listener, app).await.unwrap();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/user", get(get_user).post(post_user))
+        .route("/user/:id", get(get_user_by_id).put(put_user).delete(delete_user));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ src/routing.rs
@@ -0,0 +1,23 @@
+pub async fn root() -> &'static str {
+    "Hello, world!"
+}
+
+pub async fn get_user() -> &'static str {
+    "Get user"
+}
+
+pub async fn post_user() -> &'static str {
+    "Post user"
+}
+
+pub async fn get_user_by_id() -> &'static str {
+    "Get user by id"
+}
+
+pub async fn put_user() -> &'static str {
+    "Put user"
+}
+
+pub async fn delete_user() -> &'static str {
+    "Delete user"
+}
