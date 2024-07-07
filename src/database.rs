4df63ee44add0773ec7dabedb8a767da47c5f862
witchbl4des <menzo.smit-adam@epitech.eu>
2024-07-07 16:10:13 +0200

    fix main + db co etablished

--- src/database.rs
+++ src/database.rs
@@ -1,63 +1,49 @@
-use axum_typed_multipart::TypedMultipart;
-use chrono::prelude::*;
-use image::io::Reader;
-use passwd_auth::generate_hash;
-use ravif::{Encoder, Img, RGBA8};
-use rusqlite::{params, Connection};
-use std::fs;
-use std::io::Cursor;
-
-#[derive(Debug)]
+use rusqlite::Connection;
 
 pub fn init_db() -> Result<Connection, rusqlite::Error> {
-	let connection = Connection::open("lockedroom.db")?;
+    let connection = Connection::open("lockedroom.db")?;
+    println!("connected to database.");
 
-	//users table
-	connection.execute(
-		"CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY,
-			username TEXT NOT NULL UNIQUE,
-			password TEXT NOT NULL,
-			created_at TEXT NOT NULL
-		)",
-		[],
-	)?;
+    connection.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            created_at TEXT NOT NULL
+        )",
+        [],
+    )?;
 
-	//posts table
-	connection.execute(
-		"CREATE TABLE IF NOT EXISTS posts (
-			post_id INT NOT NULL,
-			target_id INT NOT NULL,
-			FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-			FOREIGN KEY (target_id) REFERENCES posts(id) ON DELETE CASCADE
-		)",
-		[],
-	)?;
+    connection.execute(
+        "CREATE TABLE IF NOT EXISTS posts (
+            post_id INTEGER PRIMARY KEY,
+            target_id INTEGER NOT NULL,
+            FOREIGN KEY (post_id) REFERENCES users(id) ON DELETE CASCADE,
+            FOREIGN KEY (target_id) REFERENCES users(id) ON DELETE CASCADE
+        )",
+        [],
+    )?;
 
-	//mod table
-	connection.execute(
-		"CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			username TEXT NOT NULL,
-			password TEXT NOT NULL,
-			level INT NOT NULL
-		)",
-		[],
-	)?;
+    connection.execute(
+        "CREATE TABLE IF NOT EXISTS mods (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            level INTEGER NOT NULL
+        )",
+        [],
+    )?;
 
-	//ban table
+    connection.execute(
+        "CREATE TABLE IF NOT EXISTS bans (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            reason TEXT NOT NULL,
+            created_at TEXT NOT NULL,
+            ip TEXT NOT NULL
+        )",
+        [],
+    )?;
 
-	connection.execute(
-		"CREATE TABLE IF NOT EXISTS bans (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			username TEXT NOT NULL,
-			reason TEXT NOT NULL,
-			created_at TEXT NOT NULL
-			ip TEXT NOT NULL
-		)",
-		[],
-	)?;
-
-	Ok(connection)
-	println!("Database initialized");
+    Ok(connection)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 #![recursion_limit = "512"]
 
+#![warn(unused_imports)]
 use axum::{
-	routing::{get, post},
-	Extension, Form, Router,
-	http::{Request, StatusCode},
-	response::{Html as ResponseHtml, IntoResponse, Redirect},
+    routing::{get, Router},
+    http::{StatusCode},
+    response::{Html as ResponseHtml, IntoResponse},
 };
-
-use database:: { init_db, User, Ban, Mod, Post };
+use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
+use database::init_db;
+use rusqlite::Connection;
+use serde::{Deserialize, Serialize};
+use std::{
+    net::SocketAddr,
+    sync::Arc,
+};
+use thiserror::Error;
+use tokio::sync::Mutex;
+use tower_http::services::ServeDir;
 
 mod database;
 
 #[tokio::main]
+#[warn(unused_variables)]
 async fn main() {
+    let database_conn = Arc::new(Mutex::new(init_db().unwrap()));
+    {
+        let mut conn = database_conn.lock().await;
+    }
 
-	let database_conn = Arc::new(Mutex::new(init_db().unwrap()));
-	{
-		let conn = init_db().lock().await;
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
 
-	}
-
-	let app = Router::new()
-		.route("/", get(|| async { "Hello, World!"}));
-	
-	
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-	axum::serve(listener, app).await.unwrap();
-	println!("server up on: port 3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
+    println!("server up on port: 3000");
 }
