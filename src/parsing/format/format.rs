4f20ec84e42868815954f1227a550174c569daa6
menzo <menzo.smit-adam@epitech.eu>
2024-08-28 10:44:10 +0200

    formatting functions added rn (all same patern)

--- src/parsing/format/format.rs
+++ src/parsing/format/format.rs
@@ -50,3 +50,88 @@ pub fn format_quote(input: &str) -> String {
     output
 }
 
+pub fn format_bold(input: &str) -> String {
+    let mut output = String::new();
+    let mut remaining_input = input;
+
+    while !remaining_input.is_empty() {
+        match bold_parser(remaining_input) {
+            OK((next_input, bold_text)) => {
+                output.push_str(&format!("<strong>{}</strong>", bold_text));
+                remaining_input = next_input;
+            }
+            Err(_) => {
+                let mut chars = remaining_input.chars();
+                output.push(chars.next().unwrap());
+                remaining_input = chars.as_str();
+            }
+        }
+    }
+    output
+}
+
+pub fn format_urls(input: &str) -> String {
+    let mut output = String::new();
+    let mut remaining_input = input;
+
+    while !remaining_input.is_empty() {
+        match url_parser(remaining_input) {
+            OK((next_input, url)) => {
+                output.push_str(&format!("<a href=\"{}\">{}</a>", url, url));
+                remaining_input = next_input;
+            }
+            Err(_) => {
+                let mut chars = remaining_input.chars();
+                output.push(chars.next().unwrap());
+                remaining_input = chars.as_str();
+            }
+        }
+    }
+
+    output
+}
+
+pub fn format_mentions(input: &str, conn : &Connection) -> String {
+    let mut output = String::new();
+    let mut remaining_input = input;
+
+    while !remaining_input.is_empty() {
+        match mention_parser(remaining_input) {
+            OK((next_input, mention)) => {
+                let mention_id = mention[1..].parse::<i32>().unwrap();
+                let post = get_post_by_id(conn, mention_id).unwrap();
+                output.push_str(&format!("<a href=\"/post/{}\">@{}</a>", mention_id, post.title));
+                remaining_input = next_input;
+            }
+            Err(_) => {
+                let mut chars = remaining_input.chars();
+                output.push(chars.next().unwrap());
+                remaining_input = chars.as_str();
+            }
+        }
+    }
+
+    output
+}
+
+pub fn grab_mentions(input: &str) -> Vec<i32> {
+    let mut mentions = Vec::new();
+    let mut remaining_input = input;
+
+    while !remaining_input.is_empty() {
+        match mention_parser(remaining_input) {
+            OK((next_input, mention)) => {
+                let mention_id = mention[1..].parse::<i32>().unwrap();
+                mentions.push(mention_id);
+                remaining_input = next_input;
+            }
+            Err(_) => {
+                let mut chars = remaining_input.chars();
+                chars.next();
+                remaining_input = chars.as_str();
+            }
+        }
+    }
+
+    output
+}
