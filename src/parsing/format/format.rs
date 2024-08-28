94339c4cd04d01111873365e3e8216564ab8735c
menzo <menzo.smit-adam@epitech.eu>
2024-08-28 10:41:48 +0200

    format_quote func added

--- src/parsing/format/format.rs
+++ src/parsing/format/format.rs
@@ -28,3 +28,25 @@ pub fn format_paragraph(input: &str) -> String {
 
     output
 }
+
+pub fn format_quote(input: &str) -> String {
+    let mut output = String::new();
+    let mut remaining_input = input;
+
+    while !remaining_input.is_empty() {
+        match quote_parser(remaining_input) {
+            OK((next_input, quote_text)) => {
+                output.push_str(&format!("<blockquote>{}</blockquote>", quote_text));
+                remaining_input = next_input;
+            }
+            Err(_) => {
+                let mut chars = remaining_input.chars();
+                output.push(chars.next().unwrap());
+                remaining_input = chars.as_str();
+            }
+        }
+    }
+
+    output
+}
+
